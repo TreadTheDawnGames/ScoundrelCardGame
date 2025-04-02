ee2251d4391186ad1504a199cb9413645d697569
Caspian Tyler <TreadTheDawnGames>
2025-04-01 21:21:42 -0400

    shop_overlay.gd

--- scenes/card overlays/shop_overlay.gd
+++ scenes/card overlays/shop_overlay.gd
@@ -4,4 +4,10 @@ class_name ShopOverlay
 func _ready() -> void:
 	marker_extents = get_node("MarkerExtents")
 	#AddShopCards(TDCardData_Art.SuitType.Ghosts)
+	
+	
+	
+	return
+	
+func AddCardsToShop(suit : TDCardData_Art, markerArray : Array[TDCardPositionMarker2D]) -> void:
 	return
